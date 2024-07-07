ce4b5b186d236084f068971f83f30c44b091ae34
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-07-08 00:24:29 +0300

    feat: implement filter in title, description, and meta tags

--- src/app/core/header/components/filter/filter.component.ts
+++ src/app/core/header/components/filter/filter.component.ts
@@ -1,5 +1,5 @@
 import { CustomButtonComponent } from '@/app/shared/components/custom-button/custom-button.component';
-import { SearchPipe } from '@/app/shared/pipes/search/search.pipe';
+import { FilterPipe } from '@/app/shared/pipes/filter/filter.pipe';
 import { VideoServiceService } from '@/app/shared/services/video-service/video-service.service';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -8,17 +8,17 @@ import { MatIcon } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatIcon, CustomButtonComponent, SearchPipe],
-  providers: [SearchPipe],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatIcon, CustomButtonComponent, FilterPipe],
+  providers: [FilterPipe],
   selector: 'app-filter',
   standalone: true,
   styleUrl: './filter.component.scss',
   templateUrl: './filter.component.html',
 })
 export class FilterComponent {
-  inputValue = '';
+  FilterPipe = inject(FilterPipe);
 
-  searchPipe = inject(SearchPipe);
+  inputValue = '';
 
   videoService = inject(VideoServiceService);
 
@@ -26,7 +26,7 @@ export class FilterComponent {
 
   onSubmit(): void {
     const videoItems = this.videoService.getOriginalData();
-    const newItems = this.searchPipe.transform(videoItems, this.inputValue);
+    const newItems = this.FilterPipe.transform(videoItems, this.inputValue);
     this.videoService.setUpdatedData(newItems);
   }
 }
---
+++ src/app/shared/pipes/filter/filter.pipe.ts
@@ -0,0 +1,22 @@
+import VideoItem from '@/app/core/models/video-item.model';
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter',
+  standalone: true,
+})
+export class FilterPipe implements PipeTransform {
+  transform(videos: VideoItem[], searchTerm: string): VideoItem[] {
+    if (!videos || !searchTerm) {
+      return videos;
+    }
+
+    const lowerCaseSearchTerm = searchTerm.toLowerCase();
+    return videos.filter(
+      (video) =>
+        video.snippet.title.toLowerCase().includes(lowerCaseSearchTerm) ||
+        video.snippet.description.toLowerCase().includes(lowerCaseSearchTerm) ||
+        video.snippet.tags.some((tag) => tag.toLowerCase().includes(lowerCaseSearchTerm)),
+    );
+  }
+}
