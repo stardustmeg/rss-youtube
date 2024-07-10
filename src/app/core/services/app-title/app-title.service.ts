5f4835c07f81c794b5d271d101a9cca446421631
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-07-10 16:56:43 +0300

    feat: add custom title service

---
+++ src/app/core/services/app-title/app-title.service.ts
@@ -0,0 +1,21 @@
+import { APP_TITLE } from '@/app/shared/constants/routes';
+import stringTemplate from '@/app/shared/utils/string-template';
+import { Injectable } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { RouterStateSnapshot, TitleStrategy } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppTitleService extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot): void {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(stringTemplate(APP_TITLE, { title: title.toUpperCase() }));
+    }
+  }
+}
