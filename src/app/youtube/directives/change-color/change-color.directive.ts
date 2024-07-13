05c65b37c529b375d607a0107d7c2a1fa326bc51
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-07-14 02:46:06 +0300

    refactor: remove unused method

--- src/app/youtube/directives/change-color/change-color.directive.ts
+++ src/app/youtube/directives/change-color/change-color.directive.ts
@@ -1,7 +1,6 @@
 import stringTemplate from '@/app/shared/utils/string-template';
-import { Directive, ElementRef, HostBinding, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Directive, HostBinding, Input } from '@angular/core';
 
-import { changeOption } from './constants/change';
 import { styleColorOption } from './constants/color';
 import { StyleChangeOptionType, styleChangeOption, styleOption } from './constants/style';
 import { determineColor } from './helpers/determine-color';
@@ -10,31 +9,23 @@ import { determineColor } from './helpers/determine-color';
   selector: '[appChangeColor]',
   standalone: true,
 })
-export class ChangeColorDirective implements OnChanges {
+export class ChangeColorDirective {
   @Input() public publicationDate = '';
 
   @Input() public type: StyleChangeOptionType = styleChangeOption.borderBottom;
 
-  public constructor(private el: ElementRef<HTMLElement>) {}
+  public constructor() {}
 
   private applyColor(): string {
     const color = determineColor(this.publicationDate);
     return stringTemplate(styleColorOption[this.type], { color });
   }
 
-  public ngOnChanges(changes: SimpleChanges): void {
-    if (changes[changeOption.PUBLICATION_DATE]) {
-      this.el.nativeElement.style[this.type] = this.applyColor();
-    }
-  }
-
-  @HostBinding(styleOption.background)
-  public get backgroundColor(): string {
+  @HostBinding(styleOption.background) public get backgroundColor(): string {
     return this.applyColor();
   }
 
-  @HostBinding(styleOption.borderBottom)
-  public get borderBottom(): string {
+  @HostBinding(styleOption.borderBottom) public get borderBottom(): string {
     return this.applyColor();
   }
 }
