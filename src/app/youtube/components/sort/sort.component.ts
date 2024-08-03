c2a4fa3caec76e393e748b002b19d071bef0fc74
Margarita Golubeva <margaret.golubeva@gmail.com>
2024-08-03 14:19:41 +0300

    refactor: update sorting to default on second click

--- src/app/youtube/components/sort/sort.component.ts
+++ src/app/youtube/components/sort/sort.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatChipsModule } from '@angular/material/chips';
 
 import { CustomButtonComponent } from '../../../shared/components/custom-button/custom-button.component';
+import { BASIC_SORT_OPTION } from '../../constants/sort-option';
 import { SortPipe } from '../../pipes/sort/sort.pipe';
 import { VideoDataService } from '../../services/video-data/video-data.service';
 
@@ -15,5 +16,25 @@ import { VideoDataService } from '../../services/video-data/video-data.service';
   templateUrl: './sort.component.html',
 })
 export class SortComponent {
+  private clickCounts: Record<string, number> = {
+    dateAsc: 0,
+    dateDesc: 0,
+    viewCountAsc: 0,
+    viewCountDesc: 0,
+  };
+
   public videoService = inject(VideoDataService);
+
+  public handleChipClick(chipType: string): void {
+    this.clickCounts[chipType] += 1;
+
+    if (this.clickCounts[chipType] % 2 === 0) {
+      Object.keys(this.clickCounts).forEach((key) => {
+        if (key !== chipType) {
+          this.clickCounts[key] = 0;
+        }
+      });
+      this.videoService.sortCriteria.set(BASIC_SORT_OPTION);
+    }
+  }
 }
